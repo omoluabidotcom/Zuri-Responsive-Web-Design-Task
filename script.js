c3cdf982473136d70a6db1341e870f1533bd89f5
omoluabidotcom <yyahaya222@yahoo.com>
2021-04-10 22:44:37 +0100

    All media query completed but the dropdown for the phone view hasn't work yet

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 
 function countFunc() {
     
-    document.getElementById("mydrop-content").classList.toggle("show");
+    document.getElementById("mydrop-content").classList.toggle("drop");
 }
 
 
 window.onclick = function(event) {
     if(!event.target.matches('drop-button')) {
-        var dropdowns = document.getElementsByClassName("drop-content");
+        var dropdowns = document.getElementsByClassName("nav-links");
         var i;
         for (i = 0; i < dropdowns.length; i++) {
             var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
+            if (openDropdown.classList.contains('drop')) {
+                openDropdown.classList.remove('drop');
             }
         }
     }
